d162d90ca456609133b48cde27cba457a3e012ff
Ehijele David <aigbokhaebholoelijah@gmail.com>
2026-06-11 14:40:42 +0100

    Increment version number to 4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ─── INCREMENT THIS NUMBER every time you upload a new version ───
-const VERSION = 3;
+const VERSION = 4;
 // ─────────────────────────────────────────────────────────────────
 
 const CACHE = `focusflow-v${VERSION}`;
