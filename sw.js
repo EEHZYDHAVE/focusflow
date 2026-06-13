63903647627861ea1c20440f818a2dd7d6f095fa
Ehijele David <aigbokhaebholoelijah@gmail.com>
2026-06-13 09:40:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ─── INCREMENT THIS NUMBER every time you upload a new version ───
-const VERSION = 10;
+const VERSION = 11;
 // ─────────────────────────────────────────────────────────────────
 
 const CACHE = `focusflow-v${VERSION}`;
