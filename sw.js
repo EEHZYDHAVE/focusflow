231cdeb566b9a5dc5db5cb2dbc7fbb9ef15556d2
Ehijele David <aigbokhaebholoelijah@gmail.com>
2026-07-03 19:12:46 +0100

    fixed task delete in desktop version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ─── INCREMENT THIS NUMBER every time you upload a new version ───
-const VERSION = 18;
+const VERSION = 19;
 // ─────────────────────────────────────────────────────────────────
 
 const CACHE = `focusflow-v${VERSION}`;
