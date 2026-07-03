8c777dd6bda641cc2f939184d3cdad6eedf442ed
Ehijele David <aigbokhaebholoelijah@gmail.com>
2026-07-03 10:17:40 +0100

    fixed timing clash on both mobile and desktop app

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ─── INCREMENT THIS NUMBER every time you upload a new version ───
-const VERSION = 17;
+const VERSION = 18;
 // ─────────────────────────────────────────────────────────────────
 
 const CACHE = `focusflow-v${VERSION}`;
