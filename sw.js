ae3c73c2ead7c7503fe7f5fb1ac4276f3ad7326a
Ehijele David <aigbokhaebholoelijah@gmail.com>
2026-07-10 09:41:05 +0100

    fix bug with reduced day function and added that option to mobile version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ─── INCREMENT THIS NUMBER every time you upload a new version ───
-const VERSION = 20;
+const VERSION = 21;
 // ─────────────────────────────────────────────────────────────────
 
 const CACHE = `focusflow-v${VERSION}`;
